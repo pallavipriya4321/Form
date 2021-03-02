1675e3865e1a1c9be7879fb7d455531fd9de9f65
Pallavi Priya <pallavipriya4321@gmail.com>
2021-03-03 00:44:27 +0530

    second commit

--- signup.js
+++ signup.js
@@ -1,23 +1,22 @@
-function validateemail()  
+function validate()  
 {  
     var x=document.form1.email.value;  
-   
+    var y=document.form1.mobile.value;
+    var atposition=x.indexOf("@");  
+    var dotposition=x.lastIndexOf(".");  
 
     if (atposition<1 || dotposition<atposition+2 || dotposition+2>=x.length)
     {  
-        alert("Please enter a valid e-mail address ");  
+        alert("Please enter a valid e-mail address \n atpostion:"+atposition+"\n dotposition:"+dotposition);  
         return false;  
     }  
 
-}  
-
-function validateContact()
-{
-    var y=document.form1.mobile.value;
-
-    if(y.length!==10)
+    if(y.length!=10)
     {
         alert("Enter a valid 10 digit mobile number");
-        return false;
     }
+}  
+function success()
+{
+    alert("Thank you for details, We will get back to you soon!");
 }
